f48b7ea13fe66570337217811d6eaecfe1ed92dd
devmwall <me@mattwall.dev>
2024-12-30 02:04:55 -0500

    Adding logic for the python api and ollama

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Simple word based adventure that will allow the users to travel the world and tr
 - Need to add the python api endpoint to call an ollama instance
 - Ollama config needs to be set up
 - Need to run rpi config 
+
+## Setup of Ollama
+- curl -fsSL https://ollama.com/install.sh | sh
+- ollama run tinyllama
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from ollama import chat
+from ollama import ChatResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    response: ChatResponse = chat(model='tinyllama', messages=[
+      {
+        'role': 'user',
+        'content': 'Why is the sky blue?',
+      },
+    ])
+    print(response['message']['content'])
+    # or access fields directly from the response object
+    print(response.message.content)
+    return {"message": response.message.content}
 
 # To run the application:
 # Save this script as `main.py` and use the following command:
